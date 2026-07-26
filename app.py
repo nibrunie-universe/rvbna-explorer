c8bbf39d4caadfea93baf12b9987a56f39a5aa78
Nicolas Brunie <nibrunie@gmail.com>
2026-07-26 10:49:08 -0700

    v0.0.3: * Adding text card with explanations and link to RVBNA spec

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from flask_limiter.util import get_remote_address
 app = Flask(__name__, static_folder="static")
 
 MAX_REQUESTS_PER_MINUTE = 10
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 limiter = Limiter(
     get_remote_address,
